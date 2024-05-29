8fcfbbb5ffc3a3dafc33adb7b4779adc2906b564
Van Ha <halevan0592@gmail.com>
2024-05-29 12:30:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from './Components/Header';
+import TaskList from './Components/TaskList';
 import './App.css';
+import AddTaskForm from './Components/AddTaskForm';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id:"task_1", title : "Learn Js", status: 1},
+    { id:"task_2", title : "Code a Todo List Level 2", status: 0}
+  ]);
+  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0
+      };
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+
+  const hangleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const setTaskStatus = (taskId, status) => {
+    setTasks(tasks.map(task => {
+      if (task.id === taskId) {
+        return {...task, status: status ? 1 : 0};
+      }
+      return task;
+    }))
+  }
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <Header title="Todo List" subTitle="Get one item done at a time."/>
+
+        <TaskList tasks={tasks} showIncomplete={showIncomplete} setTaskStatus={setTaskStatus} removeTask={removeTask} setShowIncomplete={setShowIncomplete}/>
+
+        <AddTaskForm newTask={newTask} handleSubmit={handleSubmit} hangleInputChange={hangleInputChange}/>
+
+
     </div>
   );
 }
